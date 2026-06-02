78cd783a77dd68f1de6b7ab183de192318ab76bd
Rick Daalhuizen <rick.daalhuizen@outlook.com>
2026-06-02 18:00:00 +0200

    init project and add README

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import type { ReactElement } from "react";
+
+const App = (): ReactElement => {
+  return (
+    <>
+      <h1>Hello Flux</h1>
+    </>
+  );
+}
+
+export default App;
