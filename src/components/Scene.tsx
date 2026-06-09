807002bb4092ea658258c2c1532175c1cc58abfe
Rick Daalhuizen <rick.daalhuizen@maxserv.com>
2026-06-09 13:06:21 +0200

    add stat.js + improve performance

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,16 +1,16 @@
-import type { ReactElement } from "react";
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Table } from "./Table.tsx";
-import { Environment } from "@react-three/drei";
+import type {ReactElement} from "react";
+import {Environment, OrbitControls, Stats } from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
+import {Table} from "./Table.tsx";
 
 const Scene = (): ReactElement => (
-    <Canvas camera={{ position: [200, 150, 250], fov: 45 }} shadows>
+    <Canvas frameloop={"demand"} camera={{ position: [200, 150, 250], fov: 45 }} shadows>
         <Environment preset="apartment" />
         <ambientLight intensity={0.4} />
         <directionalLight position={[100, 200, 100]} intensity={1.2} castShadow />
         <Table />
         <OrbitControls target={[0, 75, 0]} />
+        {import.meta.env.DEV && <Stats />}
     </Canvas>
 );
 
