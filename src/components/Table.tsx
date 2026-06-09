d87df807cd373f0ea9856e5b6c00dff6aa9839b0
Rick Daalhuizen <rick.daalhuizen@outlook.com>
2026-06-09 13:06:21 +0200

    add stat.js + improve performance

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,11 +1,14 @@
 import type { ReactElement } from "react";
 import { TableTop } from "./TableTop.tsx";
 import { TableLegs } from "./TableLegs.tsx";
+import { Suspense } from "react";
 
 const Table = (): ReactElement => (
     <group position={[0, 70, 0]}>
-        <TableTop />
-        <TableLegs />
+        <Suspense>
+            <TableTop />
+            <TableLegs />
+        </Suspense>
     </group>
 );
 
